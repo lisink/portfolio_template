572d92938078a92b4bb8e50c4e81770d71a57eb1
konstantin.lisin <sandman73@yandex.ru>
2017-03-25 00:03:03 +0400

    KLPF-2 : Revert back missing style changes. Minor content updates. Remove unused assets.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-    // place code for your default task here
-    console.log("some message");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+
+gulp.task('default', function () {
+    console.log('test echo');
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('../lisink.github.io/css'));
+});
+
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('./src/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('../lisink.github.io'));
+});
+
+gulp.task('build', ['sass', 'pug']);
