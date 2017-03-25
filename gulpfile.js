7bbb5bcf5ea4afecc3cda5f3496a2ef9b54ee00e
konstantin.lisin <sandman73@yandex.ru>
2017-03-25 23:28:29 +0400

    KLPF-2 : Configure gulp serve and watch tasks. Move import of gfonts to the stylesheet.

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,9 @@ var cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat')
+const imagemin = require('gulp-imagemin');
+
+var gls = require('gulp-live-server');
 
 gulp.task('default', function () {
     console.log('test echo');
@@ -30,6 +33,7 @@ gulp.task('csscomb', function () {
 gulp.task('build_css', function () {
     var css = gulp.src(['./src/css/**/*.css', '!./src/css/reset.css'])
         .pipe(addSrc.prepend('./src/css/reset.css'))
+        .pipe(addSrc.prepend('./src/css/google_fonts.css'))
         .pipe(sourcemaps.init());
 
     var scss = gulp.src('./src/scss/**/*.scss')
@@ -70,5 +74,36 @@ gulp.task('build_html_prod', function buildHTML() {
         .pipe(gulp.dest('../lisink.github.io'));
 });
 
+
+gulp.task('serve', function() {
+    var server = gls.static(['../lisink.github.io']);
+    server.start();
+
+    //use gulp.watch to trigger server actions(notify, start or stop)
+    gulp.watch(['../lisink.github.io/css/styles.css', '../lisink.github.io/**/*.html'], function (file) {
+        server.notify.apply(server, [file]);
+    });
+});
+
+gulp.task('watch', function() {
+    var watcher1 = gulp.watch('./src/**/*.pug', ['build_html']);
+    var watcher2 = gulp.watch('./src/scss/**/*.scss', ['build_css']);
+
+    watcher1.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+
+    watcher2.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('imagemin', () =>
+    gulp.src('../lisink.github.io/i/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('../lisink.github.io/i/min'))
+);
+
 gulp.task('build', ['build_html', 'build_css']);
 gulp.task('build_prod', ['build_html_prod', 'clean_css']);
+gulp.task('server', ['serve', 'watch']);
