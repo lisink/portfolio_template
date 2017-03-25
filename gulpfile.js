6da4999f67e6c406115b4b1d0212a9244f116cb0
konstantin.lisin <sandman73@yandex.ru>
2017-03-25 21:33:32 +0400

    KLPF-2 : Update gulp config, move css files to source in order to produce minified output.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var csscomb = require('gulp-csscomb');
+var addSrc = require('gulp-add-src');
+var merge = require('merge-stream');
+var cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat')
 
 gulp.task('default', function () {
     console.log('test echo');
@@ -14,8 +21,47 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('../lisink.github.io/css'));
 });
 
+gulp.task('csscomb', function () {
+  return gulp.src(['./src/scss/**/*.scss', '!src/scss/_*.scss'])
+    .pipe(csscomb())
+    .pipe(gulp.dest('./src/scss/'));
+});
 
-gulp.task('pug', function buildHTML() {
+gulp.task('build_css', function () {
+    var css = gulp.src(['./src/css/**/*.css', '!./src/css/reset.css'])
+        .pipe(addSrc.prepend('./src/css/reset.css'))
+        .pipe(sourcemaps.init());
+
+    var scss = gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError));
+
+    var mergedStream = merge(css, scss)
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('../lisink.github.io/css'));
+
+    return mergedStream;
+});
+
+gulp.task('autoprefixer', function () {
+    return  gulp.src('../lisink.github.io/css/styles.css')
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('../lisink.github.io/css'));
+});
+
+gulp.task('minify_css', function () {
+    return  gulp.src('../lisink.github.io/css/styles.css')
+            .pipe(cleanCSS({
+                specialComments: false,
+            }))
+            .pipe(gulp.dest('../lisink.github.io/css'));
+});
+
+gulp.task('build_html', function buildHTML() {
     return gulp.src('./src/*.pug')
         .pipe(pug({
             pretty: true
@@ -23,4 +69,11 @@ gulp.task('pug', function buildHTML() {
         .pipe(gulp.dest('../lisink.github.io'));
 });
 
-gulp.task('build', ['sass', 'pug']);
+gulp.task('build_html_prod', function buildHTML() {
+    return gulp.src('./src/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('../lisink.github.io'));
+});
+
+gulp.task('build', ['build_html', 'build_css']);
+gulp.task('build_prod', ['build_html_prod', 'autoprefixer', 'minify_css']);
